c9fc1b66b92fe5e8d179efa96d0f371828627e18
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2022-02-13 16:07:19 +0100

    Update index.example.js

--- index.example.js
+++ index.example.js
@@ -3,13 +3,13 @@ const settings = {
   file: './database.json.example'
 }
 
-database.addElement(settings.file, "Hello")
+database.add(settings.file, "Hello")
 // adds a element called "Hello"
 
-database.removeElement(settings.file, "World")
+database.remove(settings.file, "World")
 // removes a element called "World"
 
-console.log(database.getElement(settings.file, 0))
+console.log(database.get(settings.file, 0))
 // gets the first element
 
 console.log(database.getDatabase(settings.file))
